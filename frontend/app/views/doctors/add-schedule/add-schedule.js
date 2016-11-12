63616605413052672581e8173ca51b7cc3aa4f45
Marek <Marek.Myjak1221@gmail.com>
2016-11-12 13:26:44 +0100

    change daterangepicker to datetimepicker

--- frontend/app/views/doctors/add-schedule/add-schedule.js
+++ frontend/app/views/doctors/add-schedule/add-schedule.js
@@ -7,43 +7,71 @@ myApp.config(['$routeProvider', function ($routeProvider) {
     });
 }]);
 
+var isValidDate = function (date) {
+    if (Object.prototype.toString.call(date) === "[object Date]") {
+        // it is a date
+        if (isNaN(date.getTime())) {  // d.valueOf() could also work
+            console.error("date is not valid");
+            return false;
+        }
+        else {
+            // date is valid
+            return true;
+        }
+    }
+    else {
+        console.error("it is not a date");
+        return false;
+    }
+};
+
 myApp.controller('AddScheduleCtrl', ['REST_API', "$rootScope", '$scope', '$http', '$location', 'UserService',
     function (REST_API, $rootScope, $scope, $http, $location, UserService) {
         $rootScope.userDetails = UserService.getUser();
 
+        $scope.addSchedule = function () {
+            moment.locale('pl');
+            var start = new Date(moment($scope.date + " " + $scope.startTime, "D MMMM YYYY H:mm"));
+            var end = new Date(moment($scope.date + " " + $scope.endTime, "D MMMM YYYY H:mm"));
+
+            if (isValidDate(start) && isValidDate(end)) {
+                $http.post(REST_API + "doctors/" + $rootScope.userDetails.id + "/schedules", {
+                    start: start,
+                    end: end
+                })
+                    .then(function successCallback(response) {
+                        console.log("Success")
+                    }, function errorCallback(response) {
+                        console.log("[ERROR]: " + response.data.message);
+                    })
+            }
+            else{
+                console.error("Wrong data", $scope.date, $scope.startTime, $scope.endTime)
+            }
+
+        };
+
+
         $(function () {
-            $('input[name="daterange"]').daterangepicker({
-                timePicker: true,
-                timePickerIncrement: 15,
-                timePicker24Hour: true,
-                locale: {
-                    format: 'DD-MM-YYYY HH:mm'
-                }
+            $('#datePickerDiv').datetimepicker({
+                locale: 'pl',
+                format: 'D MMMM YYYY'
+            }).on("dp.change", function () {
+                $scope.date = $("#datePicker").val();
+            });
+
+            $('#startTimePickerDiv').datetimepicker({
+                locale: 'pl',
+                format: 'H:mm'
+            }).on("dp.change", function () {
+                $scope.startTime = $("#startTimePicker").val();
+            });
+
+            $('#endTimePickerDiv').datetimepicker({
+                locale: 'pl',
+                format: 'H:mm'
+            }).on("dp.change", function () {
+                $scope.endTime = $("#endTimePicker").val();
             });
         });
-
-        console.log("add-schedule", $rootScope);
-
-        $scope.addSchedule = function () {
-            if (/^(\d\d-\d\d-\d\d\d\d \d\d:\d\d - \d\d-\d\d-\d\d\d\d \d\d:\d\d)$/.test($scope.schedule)) {
-                var start = new Date($scope.schedule.match(/\d\d-\d\d-\d\d\d\d \d\d:\d\d/g)[0]);
-                var end = new Date($scope.schedule.match(/\d\d-\d\d-\d\d\d\d \d\d:\d\d/g)[1]);
-
-                console.log($scope.schedule.match(/\d\d-\d\d-\d\d\d\d \d\d:\d\d/g)[0], start, end);
-                if (start !=null && end != null) {
-                    $http.post(REST_API + "doctors/" + $rootScope.userDetails.id + "/schedules", {
-                        start: start,
-                        end: end
-                    })
-                        .then(function successCallback(response) {
-                            console.log("Success")
-                        }, function errorCallback(response) {
-                            console.log("[ERROR]: " + response.data.message);
-                        })
-                }
-            }
-            else {
-                console.log("Wrong format.");
-            }
-        }
     }]);
